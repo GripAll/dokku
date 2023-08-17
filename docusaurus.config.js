98dee82b8ee1fd72335637d7213af53977444434
GripAll <pontosdoboz@gmail.com>
2023-08-17 12:30:00 +0200

    css

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Gripbox',
-  tagline: 'Examine my previous journey',
+ // tagline: 'Examine my previous journey',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
